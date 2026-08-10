c96b15ea5db632204f6a56151765a350fa6c039d
abigail calenta <abicalenta@gmail.com>
2026-08-10 10:16:12 -0300

    bienvenida y datos del cajero

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("KIOSCO EL RECREO");
+string respuesta = Console.ReadLine();
+Console.Write("Nombre del cajero: ");
+string nombre = Console.ReadLine();
+Console.WriteLine($"Bienvenida, {nombre}. Caja abierta");
 Console.ReadLine();
